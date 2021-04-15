945720ef5fddcf68cbc65808ed3d4f498614e5bb
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-04-15 11:19:50 +0800

    [FEATURE]: web: added service worker

--- src/index.tsx
+++ src/index.tsx
@@ -21,3 +21,16 @@ startApp({
     entryElement: document.getElementById('app'),
     useError: useErrorHandler,
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log('SW registered: ', registration);
+            })
+            .catch((registrationError) => {
+                console.log('SW registration failed: ', registrationError);
+            });
+    });
+}
