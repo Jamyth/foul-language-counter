9d26d92a08ad31218fa99e42da259ce9340a9203
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-04-15 12:02:12 +0800

    [CHORE]: web: update meta

--- src/index.tsx
+++ src/index.tsx
@@ -27,7 +27,7 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('/service-worker.js')
             .then((registration) => {
-                console.log('SW registered: ', registration);
+                console.log('SW registered');
             })
             .catch((registrationError) => {
                 console.log('SW registration failed: ', registrationError);
